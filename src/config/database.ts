0611239aa69ad6b2c2cf8527ec30482233db6355
Jean Campos <jean@route.com>
2022-11-12 15:27:26 -0500

    feat: add database

---
+++ src/config/database.ts
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+async function connectDB() {
+  try {
+    const mongoURI: string =
+      "mongodb+srv://zensurance:4bUa6EAH4OQ2MMUh@cluster0.vwmxzsu.mongodb.net/?retryWrites=true&w=majority";
+    await mongoose.connect(mongoURI);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+export default connectDB;
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,9 @@
 import { app } from "./app";
+import connectDB from "./config/database";
+
 const port = 8080;
 app.listen(port, () => {
   console.log(`listening on port ${port}!`);
 });
+
+connectDB();

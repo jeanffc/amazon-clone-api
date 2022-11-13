ec62c4a01740bda2de60b961b714dd174ec693ae
Jean Campos <jean@route.com>
2022-11-13 14:24:28 -0500

    misc: update order controller

--- src/controllers/order.controller.ts
+++ src/controllers/order.controller.ts
@@ -2,27 +2,59 @@ import { Request, Response } from 'express';
 import { Order } from '../models/order.model';
 
 export const createOrder = async (req: Request, res: Response) => {
-  const order = new Order(req.body);
-  await order.save();
-  res.status(201).send(order);
+  try {
+    const order = new Order(req.body);
+    await order.save();
+    res.status(201).send(order);
+  } catch (error) {
+    res.status(400).send(error);
+  }
 };
 
 export const getOrder = async (req: Request, res: Response) => {
-  const order = await Order.findById({ _id: req.params.id });
-  res.status(200).send(order);
+  try {
+    const order = await Order.findById({ _id: req.params.id });
+    if (!order) {
+      res.status(404).send({ error: 'Order not found' });
+    }
+
+    res.status(200).send(order);
+  } catch (error) {
+    res.status(400).send(error);
+  }
 };
 
 export const getOrders = async (req: Request, res: Response) => {
-  const orders = await Order.find().sort({ created_on: -1 });
-  res.status(200).send(orders);
+  try {
+    const orders = await Order.find().sort({ created_on: -1 });
+    res.status(200).send(orders);
+  } catch (error) {
+    res.status(400).send(error);
+  }
 };
 
 export const updateOrder = async (req: Request, res: Response) => {
-  const order = await Order.findByIdAndUpdate({ _id: req.params.id }, req.body);
-  res.status(200).send(order);
+  try {
+    const order = await Order.findByIdAndUpdate({ _id: req.params.id }, req.body);
+    if (!order) {
+      res.status(404).send({ error: 'Order not found' });
+    }
+
+    res.status(200).send(order);
+  } catch (error) {
+    res.status(400).send(error);
+  }
 };
 
 export const deleteOrder = async (req: Request, res: Response) => {
-  await Order.findByIdAndDelete({ _id: req.params.id });
-  res.status(204).send();
+  try {
+    const order = await Order.findByIdAndDelete({ _id: req.params.id });
+    if (!order) {
+      res.status(404).send({ error: 'Order not found' });
+    }
+
+    res.status(204).send();
+  } catch (error) {
+    res.status(400).send(error);
+  }
 };

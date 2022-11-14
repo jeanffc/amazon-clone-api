248fe731f8e5133bd5b366b42feeaf8b2dbcf511
Jean Campos <jean@route.com>
2022-11-13 19:44:50 -0500

    feat: add slug attr to product

--- src/models/product.model.ts
+++ src/models/product.model.ts
@@ -6,6 +6,7 @@ const productSchema = new Schema(
   {
     title: { type: String, required: true },
     bodyHtml: { type: String, required: true },
+    slug: { type: String, required: true },
     image: { type: String, required: true },
     images: [String],
     variants: [

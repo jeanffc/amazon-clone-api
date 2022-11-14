6eb5586bbf4831dd1aded9ee8ccfd6abfc527bf0
Jean Campos <jean@route.com>
2022-11-14 02:31:15 -0500

    feat:

--- src/models/order.model.ts
+++ src/models/order.model.ts
@@ -7,7 +7,8 @@ const orderSchema = new Schema(
     user: { type: String, required: true, default: '1' }, // NOTE: for this exercise the user is always the same
     orderItems: [
       {
-        product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
+        productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
+        variantId: { type: String, required: true },
         title: { type: String, required: true },
         price: { type: Number, required: true },
         quantity: { type: Number, required: true, min: [1, 'Quantity can not be less then 1.'] }
